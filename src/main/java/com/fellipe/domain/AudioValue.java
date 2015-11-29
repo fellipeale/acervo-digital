cdbbf9d4ad62182ae0b78d6784ff230a6caeebcd
Fellipe Alexandre <fellipeale@gmail.com>
2015-11-29 19:33:34 -0200

    Changed the download method

--- src/main/java/com/fellipe/domain/AudioValue.java
+++ src/main/java/com/fellipe/domain/AudioValue.java
@@ -8,20 +8,56 @@ import javax.persistence.Entity;
 @Entity
 public class AudioValue extends Value {
 
-	private Byte[] value;
-	
-	public Byte[] getValue() {
-		return value;
+	private String name;
+	private String originalName;
+	private String fileType;
+	private Long size;
+	private String path;
+
+	public String getName() {
+		return name;
 	}
 
-	public void setValue(Byte[] value) {
-		this.value = value;
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getOriginalName() {
+		return originalName;
+	}
+
+	public void setOriginalName(String originalName) {
+		this.originalName = originalName;
+	}
+
+	public String getFileType() {
+		return fileType;
+	}
+
+	public void setFileType(String fileType) {
+		this.fileType = fileType;
+	}
+
+	public Long getSize() {
+		return size;
+	}
+
+	public void setSize(Long size) {
+		this.size = size;
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
 	}
 
 	@Override
 	public List<String> validate(Content content) {
 		List<String> errors = new ArrayList<String>();
-		
+
 		return errors;
 	}
 
--- src/main/java/com/fellipe/web/FileController.java
+++ src/main/java/com/fellipe/web/FileController.java
@@ -1,13 +1,22 @@
 package com.fellipe.web;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.springframework.core.io.FileSystemResource;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -19,7 +28,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileController {
 
 	private String basePath = "/tmp/acervo-digital";
-	
+
 	@RequestMapping(value = "/upload", method = RequestMethod.GET)
 	public @ResponseBody String provideUploadInfo() {
 		return "You can upload a file by posting to this same URL.";
@@ -28,19 +37,21 @@ public class FileController {
 	@RequestMapping(value = "/upload", method = RequestMethod.POST)
 	public @ResponseBody String handleFileUpload(
 			@RequestParam("libraryId") Long libraryId,
-			@RequestParam("recordId") Long recordId,
 			@RequestParam("fileName") String fileName,
 			@RequestParam("file") MultipartFile file) throws Exception {
 		if (!file.isEmpty()) {
 			try {
-				String filePath = "/" + libraryId + "/" + recordId;
-				String newFileName = new SimpleDateFormat("yyyyMMddhhmmssSSS").format(new Date())
+				String filePath = "/" + libraryId;
+				String newFileName = new SimpleDateFormat("yyyyMMddhhmmssSSS")
+						.format(new Date())
 						+ fileName.substring(fileName.lastIndexOf('.'));
 				if (!Files.exists(Paths.get(basePath + filePath))) {
 					Files.createDirectories(Paths.get(basePath + filePath));
 				}
-				file.transferTo(new File(basePath + filePath + "/" + newFileName));
-				return "{ \"path\": \"" + filePath + "/" + newFileName + "\", \"name\": \"" + newFileName + "\" }";
+				file.transferTo(new File(basePath + filePath + "/"
+						+ newFileName));
+				return "{ \"path\": \"" + filePath + "/" + newFileName
+						+ "\", \"name\": \"" + newFileName + "\" }";
 			} catch (Exception e) {
 				throw e;
 			}
@@ -49,15 +60,61 @@ public class FileController {
 					+ " because the file was empty.");
 		}
 	}
-	
-	@RequestMapping(value = "/download/{libraryId}/{recordId}/{fileName:.+}", method = RequestMethod.GET)
+
+	@RequestMapping(value = "/download/{libraryId}/{fileName:.+}", method = RequestMethod.GET)
 	@ResponseBody
-	public FileSystemResource handleFileDownload(
+	public void handleFileDownload(HttpServletResponse response,
 			@PathVariable("libraryId") Long libraryId,
-			@PathVariable("recordId") Long recordId,
-			@PathVariable("fileName") String fileName) {
-		String filePath = "/" + libraryId + "/" + recordId;
-		return new FileSystemResource(new File(basePath + filePath + "/" + fileName));
+			@PathVariable("fileName") String fileName) throws IOException {
+		String filePath = "/" + libraryId;
+
+		File file = new File(basePath + filePath + "/" + fileName);
+
+		if (!file.exists()) {
+			String errorMessage = "Sorry. The file you are looking for does not exist";
+			System.out.println(errorMessage);
+			OutputStream outputStream = response.getOutputStream();
+			outputStream.write(errorMessage.getBytes(Charset.forName("UTF-8")));
+			outputStream.close();
+			return;
+		}
+
+		String mimeType = URLConnection
+				.guessContentTypeFromName(file.getName());
+		if (mimeType == null) {
+			System.out.println("mimetype is not detectable, will take default");
+			mimeType = "application/octet-stream";
+		}
+
+		System.out.println("mimetype : " + mimeType);
+
+		response.setContentType(mimeType);
+
+		/*
+		 * "Content-Disposition : inline" will show viewable types [like
+		 * images/text/pdf/anything viewable by browser] right on browser while
+		 * others(zip e.g) will be directly downloaded [may provide save as
+		 * popup, based on your browser setting.]
+		 */
+		response.setHeader("Content-Disposition",
+				String.format("inline; filename=\"" + file.getName() + "\""));
+
+		/*
+		 * "Content-Disposition : attachment" will be directly download, may
+		 * provide save as popup, based on your browser setting
+		 */
+		// response.setHeader("Content-Disposition",
+		// String.format("attachment; filename=\"%s\"", file.getName()));
+
+		response.setContentLength((int) file.length());
+
+		InputStream inputStream = new BufferedInputStream(new FileInputStream(
+				file));
+
+		// Copy bytes from source to destination(outputstream in this example),
+		// closes both streams.
+		FileCopyUtils.copy(inputStream, response.getOutputStream());
+
 	}
 
 }
